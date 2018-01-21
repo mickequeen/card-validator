a76ecd3d1560befd5d93ef015e68195b859b123e
mickequeen <mickequeen@gmail.com>
2018-01-20 22:20:29 -0300

    funcionalidad a example

---
+++ assets/js/app.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+/*
+*validar que sólo ingresen 16 dígitos
+*/
+  $('#creditCard').keyup(function() {
+    if ($('#creditCard').val().length < 16) {
+      $('#creditCard').attr('class', 'invalid');
+      $('#status').attr('data-error', 'Favor ingrese 16 dígitos');
+    } if ($('#creditCard').val().length==16) {
+      validCard();
+    }
+  });
+/*
+*validar tarjeta
+*/
+  function validCard(){
+    var arr= [];
+    var creditCardNum = $('#creditCard').val();
+    var reverseCreditCard = arr.reverse();
+    var pairToSum = [];
+    var impairToSum = [];
+    var finalSum = [];
+    for(var i=0; i<creditCardNum.length; i++){
+      arr.push(parseInt(creditCardNum[i]));
+    }
+    var contPair = 1;
+    for (var x = 0 ; x < reverseCreditCard.length ; x++){
+      if(contPair%2==0){
+        pairToSum.push(reverseCreditCard[x]*2);
+      } else {
+        impairToSum.push(reverseCreditCard[x])
+      }
+      contPair++;
+    }
+    var pairTogether= pairToSum.join("").split("");
+    var pairAndImpair= pairTogether.concat(impairToSum);
+
+    for( var y = 0; y<pairAndImpair.length;y++){
+      //ingresar a variable y sumar cada dígito
+      finalSum+= pairAndImpair[y];
+      //si el residio de la suma/10 es 0 entonces indicar que la tarjeta es válida
+    } if (finalSum%10===0){
+      $('#creditCard').attr('class', 'valid');
+      $('#status').attr('data-success', 'Número de tarjeta Válido');
+    } else {
+    $('#creditCard').attr('class', 'invalid');
+    $('#status').attr('data-error', 'tarjeta NO válida');
+    }
+  }
+});
